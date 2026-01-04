dba25adf303ce47f22a80e7d067f7be8eda82c21
PurpleAbi <abigail.santana.medina@gmail.com>
2026-01-04 20:18:13 +0100

    removed flyway migrations

--- server-prod/src/main/java/com/myprojects/movieranking/model/SeriesEntity.java
+++ server-prod/src/main/java/com/myprojects/movieranking/model/SeriesEntity.java
@@ -17,14 +17,14 @@ public class SeriesEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    @Column(name = "abis_review")
+    @Column(name = "abis_review", columnDefinition = "TEXT")
     private String abisReview;
     @Column(name = "abis_rating", columnDefinition = "SMALLINT")
     private Integer abisRating;
     @Column(name = "year_abi_reviewed")
     private Integer yearAbiReviewed;
     private Boolean ongoing;
-    @Column (name = "poster_url")
+    @Column (name = "poster_url", length = 2048)
     private String posterUrl;
 
     public void setPosterUrl(PosterUrlData posterUrl) {
